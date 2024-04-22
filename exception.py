ab651d1fe99c7f889708806d2af888c83a1c632b
YIndusree <167734182+YIndusree@users.noreply.github.com>
2024-04-22 10:34:03 +0530

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,25 @@
+'''
+try:
+    num = 10
+    deno = 0
+    
+    result = num/deno
+    
+    print(result)
+except:
+    print("error: denominator cannot be 0")
+    
+
+
+try:
+    even_num=[2,4,6,8]
+    print(even_num[5])
+    
+except ZeroDivisionError:
+    print("denominator cannot be 0")
+    
+except IndexError:
+    print(even_num)
+'''
+
+
